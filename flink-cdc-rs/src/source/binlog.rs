efe3f4dd8f9aad3b4c546d0c72693b2262eceb5d
liuxu <liuxu@wxchina.com>
2026-09-08 10:50:19 +0800

    1.继续完善binlog的流程

--- flink-cdc-rs/src/source/binlog.rs
+++ flink-cdc-rs/src/source/binlog.rs
@@ -1,15 +1,18 @@
 use std::{
     collections::HashMap,
     fs::File,
-    intrinsics::breakpoint,
     io::{self, ErrorKind, Seek},
+    sync::Arc,
 };
 
-use mysql_binlog_connector_rust::{binlog_error::BinlogError, binlog_parser::BinlogParser};
+use mysql_binlog_connector_rust::{
+    binlog_error::BinlogError, binlog_parser::BinlogParser, event::{event_data::EventData, table_map_event::TableMapEvent},
+};
 use tokio::sync::mpsc::Sender;
 
 use crate::{
     config::{CdcConfig, source::BinlogFile},
+    mysql::schema::{TableMeta, TableSchema},
     pipeline::message::PipelineRecord,
 };
 
@@ -19,22 +22,31 @@ use crate::{
 /// 不过我们还是需要区分mysql binlog/pg binlog/oracle binlog等等
 ///
 
+type TableMetaCache = HashMap<String, HashMap<u64, Arc<TableMeta>>>;
+
 pub struct MysqlBinlogFile<'a> {
     config: &'a CdcConfig,
     binlog_file: &'a BinlogFile,
     channels: Vec<Sender<PipelineRecord>>,
+    table_schema: TableSchema,
+    table_meta_cache: TableMetaCache,
 }
 
 impl<'a> MysqlBinlogFile<'a> {
-    pub fn create(
+    pub async fn create(
         config: &'a CdcConfig,
         binlog_file: &'a BinlogFile,
         channels: Vec<Sender<PipelineRecord>>,
     ) -> Self {
+        let table_schemta = TableSchema::new(&binlog_file.url())
+            .await
+            .expect("create mysql table schema error");
         MysqlBinlogFile {
             config: config,
             binlog_file: binlog_file,
             channels: channels,
+            table_schema: table_schemta,
+            table_meta_cache: HashMap::new(),
         }
     }
 
@@ -67,7 +79,17 @@ impl<'a> MysqlBinlogFile<'a> {
                 let event_start_pos = file.stream_position().unwrap_or(0);
                 match parser.next(&mut file) {
                     Ok((header, data)) => {
-                        //todo 封装成PipelineRecord的数据格式
+                        match data {
+                            EventData::TableMap(event) => {
+                                //todo
+                            }
+                            EventData::WriteRows(event) => {}
+                            EventData::UpdateRows(event) => {}
+                            EventData::DeleteRows(event) => {}
+                            _ => {
+                                //ignore the event data
+                            }
+                        }
                     }
                     Err(BinlogError::IoError(err)) if err.kind() == ErrorKind::UnexpectedEof => {
                         if event_start_pos == file_len {
@@ -93,6 +115,10 @@ impl<'a> MysqlBinlogFile<'a> {
         }
 
         Ok(())
+    }
+
+    async fn record_table_meta(&mut self, event: TableMapEvent) {
+        let cache = self.table_meta_cache.entry(key)
     }
 }
 
